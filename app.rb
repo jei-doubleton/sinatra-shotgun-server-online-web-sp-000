1d99d5152694562a685207184280a0aaeec0d9c2
J. Singleton <jennifer.singleton@gmail.com>
2019-04-05 22:19:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ class App < Sinatra::Base
 
   get '/' do
     "Welcome to your app!!!! <br>
-    Do you want to meet me in Niagara Falls??"
+    Do you want to meet me in Niagara Falls??<br>
+    You should be changing with every refresh now."
   end
 
 end
